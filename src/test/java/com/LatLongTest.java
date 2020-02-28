13c90a41d7ccdc76d122bef2bd9507e81c62a487
Lwilson67gatech <claywilson99@att.net>
2020-02-28 10:16:34 -0500

    First Testing Push

---
+++ src/test/java/com/LatLongTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//This will test the app's latitude, longitude, and radius features
+//and whether they output valid results
+
+import org.junit.jupiter.api.Test;
+
+class LatLongTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/LoginTest.java
@@ -0,0 +1,16 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//This will test the app's login, and the values it outputs
+
+class LoginTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
